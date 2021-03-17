c02f10ca14be7da1d28cb3baf796ca6ecf1d9420
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-17 17:24:26 +0700

    Refactor effect

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -3,7 +3,8 @@ import { GameConstants } from '../constants/game.constants';
 
 export class GameScene extends Phaser.Scene {
     private giftbox: Phaser.GameObjects.Sprite;
-    private emitter: Phaser.GameObjects.Particles.ParticleEmitter;
+    private tweenGiftbox: Phaser.Tweens.Tween;
+    private fireworkEmitter: Phaser.GameObjects.Particles.ParticleEmitter[] = [];
 
     constructor() {
         super({ key: 'GameScene' });
@@ -18,19 +19,21 @@ export class GameScene extends Phaser.Scene {
     create(): void {
         this.add.image(GameConstants.GAME_CENTER_X, GameConstants.GAME_CENTER_Y, 'background');
 
-        this.animateGiftbox();
+        this.initializeGiftbox();
+        this.initializeFireworkEffect();
     }
 
     update(): void {}
 
-    private animateGiftbox(): void {
+    private initializeGiftbox(): void {
         this.giftbox = this.add.sprite(GameConstants.GAME_CENTER_X, GameConstants.GAME_CENTER_Y, 'giftbox');
         this.giftbox.scale = 0.5;
         this.giftbox.scaleX = 0.85;
         this.giftbox.setDepth(1);
+
         let ctr = 0;
 
-        this.tweens.add({
+        this.tweenGiftbox = this.tweens.add({
             targets: [this.giftbox],
             repeat: 2,
             duration: 450,
@@ -40,71 +43,36 @@ export class GameScene extends Phaser.Scene {
             onYoyo: () => {
                 ctr++;
                 if (ctr == 3) {
-                    this.fireworkEffect();
+                    this.startFireworkEffect();
                 }
             },
         });
     }
 
-    private fireworkEffect(): void {
-        const p0 = new Phaser.Math.Vector2(595, 405);
-        const p1 = new Phaser.Math.Vector2(595, 395);
-        const p2 = new Phaser.Math.Vector2(605, 395);
-        const p3 = new Phaser.Math.Vector2(605, 405);
-
-        const curve = new Phaser.Curves.CubicBezier(p0, p1, p2, p3);
-
-        const max = 28;
-        const points = [];
-        const tangents = [];
-
-        for (let c = 0; c <= max; c++) {
-            const t = curve.getUtoTmapping(c / max);
-
-            points.push(curve.getPoint(t));
-            tangents.push(curve.getTangent(t));
-        }
-
-        const tempVec = new Phaser.Math.Vector2();
-
+    private initializeFireworkEffect(): void {
         const spark0 = this.add.particles('particle');
 
-        for (let i = 0; i < points.length; i++) {
-            const p = points[i];
+        for (let i = 0; i < 72; i++) {
+            const angle = i * 5;
 
-            tempVec.copy(tangents[i]).normalizeRightHand().scale(32).add(p);
-
-            const angle = Phaser.Math.RadToDeg(Phaser.Math.Angle.BetweenPoints(p, tempVec));
-
-            spark0.createEmitter({
-                x: tempVec.x,
-                y: tempVec.y,
-                angle: angle,
-                speed: { min: 100, max: 500 },
+            const emitter = spark0.createEmitter({
+                x: GameConstants.GAME_CENTER_X,
+                y: GameConstants.GAME_CENTER_Y,
+                angle: { min: angle - 2, max: angle + 2 },
+                speed: { min: 100, max: 450 },
                 scale: { start: 0.6, end: 0.4 },
-                lifespan: 1000,
                 blendMode: 'SCREEN',
-                maxParticles: 6,
+                maxParticles: 5,
             });
+            emitter.stop();
+
+            this.fireworkEmitter.push(emitter);
         }
+    }
 
-        for (let i = 0; i < points.length; i++) {
-            const p = points[i];
-
-            tempVec.copy(tangents[i]).normalizeRightHand().scale(-32).add(p);
-
-            const angle = Phaser.Math.RadToDeg(Phaser.Math.Angle.BetweenPoints(p, tempVec));
-
-            spark0.createEmitter({
-                x: tempVec.x,
-                y: tempVec.y,
-                angle: angle,
-                speed: { min: 100, max: 500 },
-                scale: { start: 0.6, end: 0.4 },
-                lifespan: 1000,
-                blendMode: 'SCREEN',
-                maxParticles: 6,
-            });
-        }
+    private startFireworkEffect(): void {
+        this.fireworkEmitter.forEach((emitter) => {
+            emitter.start();
+        });
     }
 }
