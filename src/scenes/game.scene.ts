5d115d20b0d39e49b9657e413b1626d9a1004826
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-17 10:55:50 +0700

    Draw background and giftbox

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -1,13 +1,24 @@
 import 'phaser';
+import { GameConstants } from '../constants/game.constants';
 
 export class GameScene extends Phaser.Scene {
+    private giftbox: Phaser.GameObjects.Sprite;
+
     constructor() {
         super({ key: 'GameScene' });
     }
 
-    preload(): void {}
+    preload(): void {
+        this.load.image('background', 'assets/images/sky.jpg');
+        this.load.image('giftbox', 'assets/images/giftbox.jpg');
+    }
 
-    create(): void {}
+    create(): void {
+        this.add.image(GameConstants.GAME_WIDTH / 2, GameConstants.GAME_HEIGHT / 2, 'background');
+
+        this.giftbox = this.add.sprite(GameConstants.GAME_WIDTH / 2, GameConstants.GAME_HEIGHT / 2, 'giftbox');
+        this.giftbox.scale = 0.5;
+    }
 
     update(): void {}
 }
