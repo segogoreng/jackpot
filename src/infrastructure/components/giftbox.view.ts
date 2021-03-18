e5f7fea74ec8851e0f6c5a27d373d44d3ed793ef
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-18 18:01:57 +0700

    Added giftbox view

---
+++ src/infrastructure/components/giftbox.view.ts
@@ -0,0 +1,27 @@
+export class GiftBoxView {
+    private sprite: Phaser.GameObjects.Sprite;
+    private text: Phaser.GameObjects.Text;
+
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        this.sprite = scene.add.sprite(x, y, 'giftbox').setInteractive();
+        this.sprite.scale = 0.25;
+        this.sprite.scaleX = 0.4;
+        this.sprite.setDepth(1);
+
+        this.text = scene.add.text(x, y, '', {
+            fontSize: '64px',
+            align: 'center',
+            color: 'black',
+        });
+        this.text.setOrigin(0.5);
+        this.text.setDepth(2);
+    }
+
+    getSprite(): Phaser.GameObjects.Sprite {
+        return this.sprite;
+    }
+
+    setPrize(prize: number) {
+        this.text.setText(prize.toString());
+    }
+}
