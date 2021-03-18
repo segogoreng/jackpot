9376e80f3fd8d28263206ba6c53bd2cf7549970e
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-18 10:59:05 +0700

    Added constants

---
+++ src/constants/display.constants.ts
@@ -0,0 +1,6 @@
+export class DisplayConstants {
+    static readonly GAME_WIDTH = 1200;
+    static readonly GAME_HEIGHT = 800;
+    static readonly GAME_CENTER_X = DisplayConstants.GAME_WIDTH / 2;
+    static readonly GAME_CENTER_Y = DisplayConstants.GAME_HEIGHT / 2;
+}
--- src/constants/game.constants.ts
+++ src/constants/game.constants.ts
@@ -1,6 +1,3 @@
 export class GameConstants {
-    static readonly GAME_WIDTH = 1200;
-    static readonly GAME_HEIGHT = 800;
-    static readonly GAME_CENTER_X = GameConstants.GAME_WIDTH / 2;
-    static readonly GAME_CENTER_Y = GameConstants.GAME_HEIGHT / 2;
+    static readonly NUM_OF_BOXES = 9;
 }
