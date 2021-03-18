f77b7ea125ce1209b187b7a20b10bbf960134448
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-18 10:59:34 +0700

    Move game config

---
+++ src/infrastructure/config/game.config.ts
@@ -0,0 +1,10 @@
+import { DisplayConstants } from '../../constants/display.constants';
+import { GameScene } from '../scenes/game.scene';
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: DisplayConstants.GAME_WIDTH,
+    height: DisplayConstants.GAME_HEIGHT,
+    parent: 'game',
+    scene: [GameScene],
+};
