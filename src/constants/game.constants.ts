145584f5f123fd1c38c74d6a600e8506d457b3ef
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-18 18:00:58 +0700

    Added game constants

--- src/constants/game.constants.ts
+++ src/constants/game.constants.ts
@@ -1,4 +1,9 @@
 export class GameConstants {
-    static readonly NUM_OF_BOXES = 9;
+    static readonly BOX_ROWS = 3;
+
+    static readonly BOX_COLS = 3;
+
+    static readonly NUM_OF_BOXES = GameConstants.BOX_ROWS * GameConstants.BOX_COLS;
+
     static readonly PRIZES = Object.freeze([0, 50, 100, 150, 200, 500, 1000, 100000]);
 }
