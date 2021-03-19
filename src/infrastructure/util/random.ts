1eeadb63c52ce69498075ddc6038371eb8274834
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-19 09:33:04 +0700

    Add implementation for random functions using random library

---
+++ src/infrastructure/util/random.ts
@@ -0,0 +1,12 @@
+import { IRandom } from '../../domain/random.interface';
+import { Random as RandomLib } from '../../lib/random';
+const randomLib: RandomLib = require('../../lib/random');
+
+export class Random implements IRandom {
+    int(min: number, max: number): number {
+        return randomLib.int(min, max);
+    }
+    normal(mean: number, standardDeviation: number): () => number {
+        return randomLib.normal(mean, standardDeviation);
+    }
+}
