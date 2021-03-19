ccf4d525fd768cc7c2f34c371b8d3ed85130493e
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-03-19 14:34:36 +0700

    Use random lib

--- src/infrastructure/util/random.ts
+++ src/infrastructure/util/random.ts
@@ -1,12 +1,13 @@
 import { IRandom } from '../../domain/random-interface';
-import { Random as RandomLib } from 'seedrandom';
+import { Random as RandomLib } from 'random';
+const randomLib: RandomLib = require('random');
 
 export class Random implements IRandom {
     public int(min: number, max: number): number {
-        return RandomLib.int(min, max);
+        return randomLib.int(min, max);
     }
 
     public normal(mean: number, standardDeviation: number): () => number {
-        return RandomLib.normal(mean, standardDeviation);
+        return randomLib.normal(mean, standardDeviation);
     }
 }
